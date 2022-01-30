0c1b1f262f0053ce5a2a18e1f4f013ff2b70c029
luminoleon <luminoleon@outlook.com>
2022-01-30 23:13:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# scf-lighthouse-snapshot
+
+利用云函数，自动为腾讯轻量应用服务器创建快照。
---
+++ index.py
@@ -0,0 +1,82 @@
+import json
+import os
+
+from tencentcloud.common import credential
+from tencentcloud.common.profile.client_profile import ClientProfile
+from tencentcloud.common.profile.http_profile import HttpProfile
+from tencentcloud.lighthouse.v20200324 import lighthouse_client, models
+
+
+SNAPSHOT_NAME = "Snapshot-Created-by-SCF"
+
+
+class LighthouseSnapshotManager:
+    def __init__(self, secret_id, secret_key, region, instance_id):
+        self.secret_id = secret_id
+        self.secret_key = secret_key
+        self.region = region
+        self.instance_id = instance_id
+
+        cred = credential.Credential(secret_id, secret_key)
+        httpProfile = HttpProfile()
+        httpProfile.endpoint = "lighthouse.tencentcloudapi.com"
+        clientProfile = ClientProfile()
+        clientProfile.httpProfile = httpProfile
+        client = lighthouse_client.LighthouseClient(cred, region, clientProfile)
+        self.client = client
+    
+    def describe(self, snapshot_name = None):
+        req = models.DescribeSnapshotsRequest()
+        params = {
+            "Filters": [
+                {
+                    "Name": "instance-id",
+                    "Values": [ self.instance_id ]
+                }
+            ]
+        }
+        if snapshot_name:
+            params["Filters"].append(
+                {
+                    "Name": "snapshot-name",
+                    "Values": [ snapshot_name ]
+                }
+            )
+        req.from_json_string(json.dumps(params))
+        resp = self.client.DescribeSnapshots(req)
+        return resp.SnapshotSet
+
+    def create(self, snapshot_name):
+        req = models.CreateInstanceSnapshotRequest()
+        params = {
+            "InstanceId": "lhins-mdickw80",
+            "SnapshotName": snapshot_name
+        }
+        req.from_json_string(json.dumps(params))
+        self.client.CreateInstanceSnapshot(req)
+
+    def delete(self, snapshot_name):
+        snapshots = self.describe(snapshot_name)
+        if len(snapshots) > 0:
+            snapshot_id = snapshots[0].SnapshotId
+            req = models.DeleteSnapshotsRequest()
+            params = {
+                "SnapshotIds": [ snapshot_id ]
+            }
+            req.from_json_string(json.dumps(params))
+            self.client.DeleteSnapshots(req)
+
+
+def main_handler(event, context) -> None:
+    secret_id = os.environ.get("SECRET_ID")
+    secret_key = os.environ.get("SECRET_KEY")
+    region = os.environ.get("REGION")
+    instance_id = os.environ.get("INSTANCE_ID")
+
+    snapshot_manager = LighthouseSnapshotManager(secret_id, secret_key, region, instance_id)
+    if len(snapshot_manager.describe(SNAPSHOT_NAME)) > 0:
+        print("检测到旧快照存在")
+        snapshot_manager.delete(SNAPSHOT_NAME)
+        print("删除旧快照成功")
+    snapshot_manager.create(SNAPSHOT_NAME)
+    print(f"创建新快照成功。快照名称：{SNAPSHOT_NAME}")
